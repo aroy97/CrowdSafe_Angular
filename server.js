75f567133296a7237a8615f1777bea75d32d674f
Akash Roy <akash.roy.ece19@heritageit.edu.in>
2020-04-20 12:56:30 +0530

    Added final

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const path = require('path');
 const app = express();   
 
 // Serve only the static files form the dist directory    
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/Covid19'));
 
 app.get('/*', function(req,res) {  
-    res.sendFile(path.join(__dirname+'/dist/index.html'));   
+    res.sendFile(path.join(__dirname+'/dist/Covid19/index.html'));   
 });  
 
 // Start the app by listening on the default Heroku port    
